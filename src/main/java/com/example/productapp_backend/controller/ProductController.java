9244cf1be17ebaa3ca18fa10953b5f28035cac71
HarshaSalih <harshasalih2000@gmail.com>
2022-12-05 18:52:57 +0530

    add commits

--- src/main/java/com/example/productapp_backend/controller/ProductController.java
+++ src/main/java/com/example/productapp_backend/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.example.productapp_backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,11 @@ public class ProductController {
     public String Homepage()
     {
         return ("Welcome to product app homepage");
+    }
+
+    @PostMapping("/add")
+    public String productadd()
+    {
+        return ("Welcome to product add page");
     }
 }
